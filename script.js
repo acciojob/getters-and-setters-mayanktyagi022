8dffb8f2b041314fab03a0ad068bf33fc4a2c18c
mayanktyagi022 <mayank.tyagiy3k@gmail.com>
2025-11-10 15:29:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 //complete this code
-class Person {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
+  get width() {
+    return this._width;
+  }
+  get height() {
+    return this._height;
+  }
+  getArea() {
+    return this._width * this._height;
+  }
+}
 
-class Student extends Person {}
-
-class Teacher extends Person {}
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);
+  }
+  getPerimeter() {
+    return this._width * 4;
+  }
+}
 
 // Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+window.Rectangle = Rectangle;
+window.Square = Square;
