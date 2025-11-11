a2e671d3aefada1288226c6a2e07b25ce3521e4b
mayanktyagi022 <mayank.tyagiy3k@gmail.com>
2025-11-11 17:57:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-// Base class Person
+
 class Person {
   constructor(name, age) {
     this._name = name;
     this._age = age;
   }
 
-  // Getter for name
   get name() {
     return this._name;
   }
 
-  // Getter for age
+  
   get age() {
     return this._age;
   }
 
-  // Setter for age
+
   set age(newAge) {
     this._age = newAge;
   }
@@ -28,7 +27,6 @@ class Student extends Person {
   }
 }
 
-// Subclass Teacher
 class Teacher extends Person {
   teach() {
     console.log(`${this.name} is teaching`);
